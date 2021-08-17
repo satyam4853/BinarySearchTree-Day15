786b818b4fec3f17700b25271dec7a2541c85cd6
Your Name <you@example.com>
2021-08-17 18:56:44 +0530

    feat: Creating BST by Adding Numbers

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,11 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("----------Welcome To Binary Search Tree Problem-----------");
+            BTree<int> binarySearch = new BTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
         }
     }
 }
