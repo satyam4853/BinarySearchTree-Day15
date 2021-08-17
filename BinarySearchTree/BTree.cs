64ce5859445824a9ba89b933d03f9b157e8b304b
Your Name <you@example.com>
2021-08-17 18:59:58 +0530

    feat: Adding Numbers Using Size Method

--- BinarySearchTree/BTree.cs
+++ BinarySearchTree/BTree.cs
@@ -17,6 +17,10 @@ namespace BinarySearchTree
         }
         int leftCount = 0, rightCount = 0;
         bool result = false;
+        /// <summary>
+        /// Insert the value in Binary Tree
+        /// </summary>
+        /// <param name="item"></param>
         public void Insert(T item)
         {
             T currentNodeValue = this.NodeData;
@@ -35,6 +39,9 @@ namespace BinarySearchTree
                     this.RightTree.Insert(item);
             }
         }
+        /// <summary>
+        /// Display the elements of Binary Tree
+        /// </summary>
         public void Display()
         {
             if (this.LeftTree != null)
@@ -43,16 +50,14 @@ namespace BinarySearchTree
                 this.LeftTree.Display();
             }
             Console.WriteLine(this.NodeData.ToString());
-            if (this.RightTree != null)
+            if (RightTree != null)
             {
                 this.rightCount++;
                 this.RightTree.Display();
+
             }
         }
-        public void GetSize()
-        {
-            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
-        }
+        public void GetSize() => Console.WriteLine("Size" + " " + (1 + leftCount + rightCount));
     }
 
 }
